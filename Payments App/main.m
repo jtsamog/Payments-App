86388c5cacb8a8aaaa50881a7ba1ade72dc03565
Sam Ogunyowa <mailsamog@gmail.com>
2017-06-29 21:12:42 -0400

    Initial Commit

---
+++ Payments App/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Payments App
+//
+//  Created by Endeavour2 on 6/29/17.
+//  Copyright © 2017 SamOg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
